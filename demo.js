e64892e33b34ca0fd1857794ebe1a63ec235f33d
rasankardipali <rasankardipali@gmail.com>
2022-10-04 17:34:47 +0530

    update demo.js file
    update demo.js file

--- demo.js
+++ demo.js
@@ -3,3 +3,8 @@ console.log("Rasankar Diapli");
 console.log("this is information");
 console.warn(" my college");
 console.error(" this is error");// my 1 error
+console.info(" My hobbies are cooking,drawing,playing games,dancing ect");
+console.log(30+50);
+console.log(49*479);
+console.log(234/20);
+console.log(1000-500);
