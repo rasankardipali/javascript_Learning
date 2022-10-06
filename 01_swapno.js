78b394278c4ba804a1e8ff3f1f9a555ee683ea91
rasankardipali <rasankardipali@gmail.com>
2022-10-06 08:57:50 +0530

    01_variable & swap no
    01_variable & swap no

---
+++ 01_swapno.js
@@ -0,0 +1,9 @@
+
+ var nam1=100;
+var nam2=200;
+var temp;
+temp=nam1;
+nam1=nam2;
+ 
+ nam2=temp;
+ console.log(nam1,nam2);
---
+++ 01_variable.js
@@ -0,0 +1,7 @@
+var my_apple;//declaration
+my_apple= " Mongo"; //initilization
+var my_apple="apple";
+my_name="dipali";
+my_name="Diapli Rasankar";
+console,console.log( "Mango");
+console.log(my_name);
