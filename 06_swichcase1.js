f2089b125698c14378b945e638a1a39cc82b481c
rasankardipali <rasankardipali@gmail.com>
2022-10-14 13:30:21 +0530

    today`s class file & assignment are pussed
    today`s class file & assignment are pussed

---
+++ 06_swichcase1.js
@@ -0,0 +1,47 @@
+//Q1 find month between 12 month...
+
+var month = 13;
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("Fedruary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("JUly");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+
+
+    default:
+        console.log("invalid inpute");
+        break;
+}
+
---
+++ 07_forloopAssignment.js
@@ -0,0 +1,38 @@
+//Q1 print no. from5-15
+
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+    
+}
+console.log("*************************************");
+//Q2 print no. from 50-40
+for (let test =50; test>=40; test--) {
+    console.log(test);
+    
+}
+console.log("*************************************");
+//Q3 find first 15 odd number
+for (let odd = 0; odd <=30; odd=odd+2) {
+    console.log(odd);
+    
+    
+}
+console.log("*************************************");
+//Q4 table of 5
+for (let table = 5; table <=50; table=table+5) {
+    console.log(table);
+    
+    
+}
+console.log("*************************************");
+//Q5 print table of 10..
+for (let num = 10; num <=100; num=num+10) {
+    console.log(num);
+    
+}
+console.log("*************************************");
+//Q5 print 10 table in reverse order
+for (let ten = 100; ten >=10; ten=ten-10) {
+    console.log(ten);
+    
+}
